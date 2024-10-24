84a7602816f9b79cd25f2c9079ded339bc5256fa
NicoGartmann <nico.gartmann97+github@gmail.com>
2024-10-24 12:29:01 +0200

    feat(): added metalllb
    - metalllb is used to provide a LoadBalancer for the cluster.
    - the aim is to configure one ip address to point to the lb which
    forwards the traffic to the ingress-controller traefik

---
+++ metalllb.tf
@@ -0,0 +1,33 @@
+resource "helm_release" "metallb" {
+  name       = "metallb"
+  repository = "https://metallb.github.io/metallb"
+  chart      = "metallb"
+  version    = "0.14.8"
+  namespace  = "metallb"
+  create_namespace = true
+
+  values = [
+    templatefile("${path.module}/templates/metallb.yml.tftpl",{})
+  ]
+}
+
+resource "kubernetes_manifest" "metalllb-config" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "namespace" = "metallb"
+      "name" = "metallb-config"
+    }
+    "data" = {
+      "config" = <<EOF
+address-pools:
+  - name: default
+    protocol: layer2
+    addresses:
+      - 192.168.3.11-192.168.3.11
+EOF
+    }
+  }
+}
+
