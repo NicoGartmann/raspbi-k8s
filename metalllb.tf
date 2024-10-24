7f86e90fc2f081f90809105dac4f499f3ccd0386
NicoGartmann <nico.gartmann97+github@gmail.com>
2024-10-24 22:21:46 +0200

    fix(metallb): the external loadbalancer should be available unter 192.168.3.100

--- metalllb.tf
+++ metalllb.tf
@@ -11,23 +11,50 @@ resource "helm_release" "metallb" {
   ]
 }
 
-resource "kubernetes_manifest" "metalllb-config" {
+resource "kubernetes_manifest" "ip_address_pool" {
   manifest = {
-    "apiVersion" = "v1"
-    "kind"       = "ConfigMap"
+    "apiVersion" = "metallb.io/v1beta1"
+    "kind" = "IPAddressPool"
     "metadata" = {
+      "name" = "example"
       "namespace" = "metallb"
-      "name"      = "metallb-config"
     }
-    "data" = {
-      "config" = <<EOF
-address-pools:
-  - name: default
-    protocol: layer2
-    addresses:
-      - 192.168.3.11-192.168.3.11
-EOF
+    "spec" = {
+      "addresses" = [
+        "192.168.3.100/32",
+      ]
     }
   }
 }
+
+resource "kubernetes_manifest" "l2_advertisement" {
+  manifest = {
+    "apiVersion" = "metallb.io/v1beta1"
+    "kind" = "L2Advertisement"
+    "metadata" = {
+      "name" = "empty"
+      "namespace" = "metallb"
+    }
+  }
+}
+
+#resource "kubernetes_manifest" "metalllb-config" {
+#  manifest = {
+#    "apiVersion" = "v1"
+#    "kind"       = "ConfigMap"
+#    "metadata" = {
+#      "namespace" = "metallb"
+#      "name"      = "metallb-config"
+#    }
+#    "data" = {
+#      "config" = <<EOF
+#address-pools:
+#  - name: default
+#    protocol: layer2
+#    addresses:
+#      - 192.168.3.100-192.168.3.100
+#EOF
+#    }
+#  }
+#}
 
